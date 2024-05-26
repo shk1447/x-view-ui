b0a45b76fdfcd05618e72fd50971aa40292c5162
shkim <sukan8822@gmail.com>
2024-05-27 02:18:53 +0900

    clear

--- src/components/molecules/Sidebar/MenuArea/Logo/Logo.tsx
+++
@@ -1,7 +0,0 @@
-import { styled } from '@mui/material/styles';
-import _SvgIcon from '@mui/material/SvgIcon';
-import { IconProps } from '../interfaces';
-
-const Logo = styled(_SvgIcon)<IconProps>(({ theme }) => ({}));
-
-export default Logo;
--- src/components/molecules/Sidebar/MenuArea/interfaces.ts
+++
@@ -1,9 +0,0 @@
-import { SvgIconProps } from "@mui/material/SvgIcon";
-
-export interface IconProps extends SvgIconProps {
-  component: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & {
-      title?: string | undefined;
-    }
-  >;
-}
